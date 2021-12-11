677b6a5c8ac632b5010ae219231e74b15bf40ab3
ankit9327 <nagar09ankit@gmail.com>
2021-12-11 18:51:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import UseEffectDemo1 from './UseEffectDemo1'
+
+export default function App() {
+  return (
+    <div>
+     <UseEffectDemo1/>
+    </div>
+  );
+}
---
+++ src/UseEffectDemo1.js
@@ -0,0 +1,21 @@
+import React, { useEffect,useState } from 'react';
+import './style.css';
+
+const UseEffctDemo1=()=> {
+
+  const [count,setCount]=useState(0);
+useEffect(()=>{
+  document.title=`chats(${count})`
+})
+
+ useEffect(()=>{
+   console.log("useEffect")
+ })
+  return (
+    <div>
+      <h1>Hello useeffect {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Click me</button>
+    </div>
+  );
+}
+export default UseEffctDemo1
