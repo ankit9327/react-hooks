07fd5c90ebf0b8763df583bb49bdc6d77f5566d9
ankit9327 <nagar09ankit@gmail.com>
2021-12-12 00:25:52 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 import UseEffectDemo1 from './UseEffectDemo1'
+import UseEffectApi from './Components/UseEffectApi'
 
 export default function App() {
   return (
     <div>
-     <UseEffectDemo1/>
+     {/* <UseEffectDemo1/> */}
+     <UseEffectApi/>
     </div>
   );
 }
